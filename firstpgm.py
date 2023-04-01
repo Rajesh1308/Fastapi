4e2f2da907bba63c5f84c96f8556ab7496b39cd3
Rajesh1308 <rajesharulrathna@gmail.com>
2023-04-01 15:41:03 +0530

    Finised the project and tested in local host

--- firstpgm.py
+++ firstpgm.py
@@ -1,49 +1,30 @@
 from fastapi import FastAPI, Path
+import test_rbf as trbf
+import pickle
+import Feature_extraction_new as feature
+
+
 app = FastAPI()
 
-inventory = { 
-    1 : {
-    "Name" : "tea Powder",
-    "Price ": 96.00,
-    "Quantity" : 1000
-    },
-    2 : {
-    "Name" : "Coffee",
-    "Price": 102.00,
-    "Quantity" : 2065
-    }
-}
+with open("model_svm_rbf.pkl", "rb") as f:
+    model_svm_rbf = pickle.load(f)
 
 @app.get("/")
 def home():
     a = 5
     return {"Data" : "Testing", "value" : a}
 
-@app.get("/about")
-def about():
-    return {"Name" : "Rajesh A"}
-
-@app.get("/get_item/{item_id}")
-def get_item(item_id: int = Path(description="The ID of the item you wish",gt=0,lt=3)):
-    return inventory[item_id]
-
-@app.get("/leap/{year}")
-def leap(year: int):
-    if (year%4 == 0):
-        ans = "It is a leap year"
-    else:
-        ans = "It is not a leap year"
-    return ans
-
-@app.get("/get-by-name")
-def get_item(Name: str):
-    for item_id in inventory:
-        if inventory[item_id]["Name"] == Name:
-            return inventory[item_id]
-    return {"Data " : "Not found"}
 
 @app.get("/url-detect")
-def get_item(Name: str):
-    print(Name)
+def get_item(url: str):
 
-    return {'URL ' : Name}
+    y_for_test = feature.get_data_set(url)
+    val = y_for_test.fillna(0)
+    pred = model_svm_rbf.predict(val)
+    print(pred)
+    if pred[0] == 1:
+        res = "Legitimate website"
+    else:
+        res = "Phished"
+
+    return res
