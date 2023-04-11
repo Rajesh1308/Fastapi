ba3b669d5e0073e900e0c101d745a1e259c19314
Rajesh1308 <rajesharulrathna@gmail.com>
2023-04-11 22:02:22 +0530

    Removed ML project

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Path
+import uvicorn
+import os
+import subprocess
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    a = 5
+    return {"Result" : "The site is up and running"}
+
+
+@app.get("/leap-year")
+def get_item(year: int):
+    if year%4 == 0:
+        res = "It is a leap year"
+    else:
+        res = "It is not a leap year"
+    return res
+
+@app.get("/path-param")
+def path_param(value):
+    res = os.system(value)
+    returned_text = subprocess.check_output(value, shell=True, universal_newlines=True)
+    return {"The value you entered in path is " : returned_text}
+
+if '__main__' == __name__:
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
